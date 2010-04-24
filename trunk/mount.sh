d2cca6835d61b41cebcb8f28496bdda2c9459533
allen.porter <allen.porter@8a0cf5cb-4335-0410-ab3f-6757f7ef3525>
2010-04-24 18:06:03 +0000

    Remove the trunk, will get replaced with iphonedisk2 branch.

--- trunk/mount.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Auhor: Allen Porter <allen@thebends.org>
-
-mkdir /Volumes/iPhone
-./iphonedisk_mount -o defer_permissions -o volname=iPhone -o volicon=./iPhoneDisk.icns /Volumes/iPhone
